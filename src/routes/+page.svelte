7d0a475cea7fca50c7e4c57f6646bcabd6da3633
thisisaarush <tanwaraarush007@gmail.com>
2023-12-10 21:36:04 +0530

    added tailwind css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex min-h-screen flex-col items-center justify-center gap-6">
+	<img src="/svelte.svg" alt="svelte icon" width="80px" />
+
+		<p class="text-2xl font-bold text-orange-600">SvelteKit</p>
+	
+	<p>
+		Visit <a class="underline underline-offset-2" href="https://kit.svelte.dev">kit.svelte.dev</a> to
+		read the documentation
+	</p>
+</div>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: black;
+		color: white;
+	}
+</style>
