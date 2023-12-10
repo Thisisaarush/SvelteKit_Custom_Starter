186f2e634fcd820c8bcd1c774efb57b7dfb1cdd8
thisisaarush <tanwaraarush007@gmail.com>
2023-12-10 23:53:59 +0530

    added flowbite ui library

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,16 @@
+<script>
+	import { Alert } from 'flowbite-svelte'
+</script>
+
 <div class="flex min-h-screen flex-col items-center justify-center gap-6">
 	<img src="/svelte.svg" alt="svelte icon" width="80px" />
-
-		<p class="text-2xl font-bold text-orange-600">SvelteKit</p>
-	
-	<p>
-		Visit <a class="underline underline-offset-2" href="https://kit.svelte.dev">kit.svelte.dev</a> to
-		read the documentation
-	</p>
+	<h1 class="text-2xl font-bold text-orange-600">SvelteKit</h1>
+	<Alert color='green' class='py-2 rounded-full'>
+		<p>
+			Visit <a class="underline underline-offset-2" href="https://kit.svelte.dev">kit.svelte.dev</a> to
+			read the documentation
+		</p>
+	</Alert>
 </div>
 
 <style lang="postcss">
